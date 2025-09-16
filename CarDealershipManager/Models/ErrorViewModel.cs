6fef99fa383d363320c7e997bede6642d096d3d6
Lila M. <lila.msfrazao@gmail.com>
2025-09-16 12:10:49 -0300

    Initial MVC Project Setup

---
+++ CarDealershipManager/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CarDealershipManager.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
